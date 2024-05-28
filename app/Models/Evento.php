56458e8cfe64b5ba60e69dd715b50520dc6c58f2
GabrieleGarofalo <124597681+GabrieleGarofalo@users.noreply.github.com>
2024-05-28 16:06:41 +0200

    creazione del modello e controller e api evento persona

---
+++ app/Models/Evento.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Evento extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['nome' , 'data'];
+
+    public function personas()
+    {
+        return $this->hasMany(Persona::class);
+    }
+}
